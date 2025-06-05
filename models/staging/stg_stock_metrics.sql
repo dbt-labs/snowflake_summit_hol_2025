fedb5497f190762d4e01396e301f6eda26d051e8
Jude Fernandez <jude.fernandez@dbtlabsworkshop.com>
2025-06-05 19:51:58 +0000

    WIP

--- models/staging/stg_stock_metrics.sql
+++ models/staging/stg_stock_metrics.sql
@@ -1,9 +1,9 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
+{{ config(
+    materialized="dynamic_table",
+    target_lag="1 minutes",
+    snowflake_warehouse="transformer_coalesce",
+    refresh_mode="FULL"
+) }}
 with source as (
     select * from {{ source('stock_data', 'US_STOCK_METRICS') }}
 ),
@@ -22,3 +22,4 @@ renamed as (
 
 select * from renamed 
 where run_date >= '2024-01-01'
+and ticker is not null 
